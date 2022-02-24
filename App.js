9ae9e74197d79724c4f99195500b63062ffd6cc5
Logesh2803 <logeshlogesh331@gmail.com>
2022-02-24 13:26:28 +0530

    Multilingual Added

--- App.js
+++ App.js
@@ -1,27 +1,112 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React from 'react';
+import {
+  SafeAreaView,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import './src/hooks/i18n.js';
+import { useTranslation } from 'react-i18next';
 
-import React from 'react'
-import Calculator from './Components/Calculator';
-function App() {
+export default function App() {
+  const { t, i18n } = useTranslation();
+  const changeLanguage = (langCode) => {
+    console.log("Language code")
+    i18n
+      .changeLanguage(langCode)
+      .then(() => console.log("language changed successfully"))
+      .catch(err => console.log("Error msg", err))
+  }
+
   return (
-    <Calculator />
-  );
-}
-export default App;
+    <>
+    <SafeAreaView style={{
+      paddingTop: 190,
+      paddingHorizontal: 16
+    }}>
+      
+      <View>
+        <Text style={{
+          color: '#8252B1',
+          fontSize: 28,
+          fontWeight: 'bold',
+          textAlign: 'center'
+        }}>
+          {t('translations:Hello')}
+        </Text>
+        <TouchableOpacity onPress={() => changeLanguage('en')}
+          style={{
+            backgroundColor: 'powderblue',
+            marginTop: 10,
+            paddingHorizontal: 10
+          }}>
+          <Text style={{
+            color: '#444',
+            fontSize: 28,
+            fontWeight: '600'
+          }}>
+            English
+          </Text>
+        </TouchableOpacity>
+      </View>
 
+      <View>
+        <TouchableOpacity onPress={() => changeLanguage('tn')}
+          style={{
+            backgroundColor: 'tomato',
+            marginTop: 10,
+            paddingHorizontal: 10
+          }}>
+          <Text style={{
+            color: '#444',
+            fontSize: 28,
+            fontWeight: '600'
+          }}>
+            தமிழ்
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <View>
+        <TouchableOpacity onPress={() => changeLanguage('fn')}
+          style={{
+            backgroundColor: 'powderblue',
+            marginTop: 10,
+            paddingHorizontal: 10
+          }}>
+          <Text style={{
+            color: '#444',
+            fontSize: 28,
+            fontWeight: '600'
+          }}>
+            Français
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+
+      <View>
+        <TouchableOpacity onPress={() => changeLanguage('hn')}
+          style={{
+            backgroundColor: 'tomato',
+            marginTop: 10,
+            paddingHorizontal: 10
+          }}>
+          <Text style={{
+            color: '#444',
+            fontSize: 28,
+            fontWeight: '600'
+          }}>
+            हिन्दी
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+
+    </SafeAreaView>
+    </>
+  );
+};
 
 
 
