844dcf15bd01d25106eb35478427837d0fc5edab
Logesh2803 <logeshlogesh331@gmail.com>
2022-02-24 11:31:31 +0530

    Final changes done

--- Components/Calculator.js
+++ Components/Calculator.js
@@ -109,153 +109,27 @@
 // })
 // export default Calculator;
 
-// import React, { useState } from 'react';
-// import { Text, View, StyleSheet, TouchableOpacity,TextInput } from 'react-native'
-// import Results from './Results';
-// import DateField from 'react-native-datefield';
-// import {useForm, Controller} from 'react-hook-form';
-// function Calculator() {
-//     const {
-//         control,
-//         handleSubmit,
-//         formState: {errors},
-//       } = useForm({
-//         defaultValues: {
-//           firstName: '',
-         
-//         },
-//       });
-//       const onSubmit = data => console.log(data);
-//     const [data, setData] = useState({
-//         date: '',
-//         birthday: '',
-        
-//         showResults: false,
-//     });
-//     const CalculateAge = () => {
-//         setData({ birthday: data.date, showResults: true });
-//     };
-//     const changeHandler = (value) => {
-//         setData({ date: value });
-//     };
-//     const nameHandler =() => {
-//         setData({firstName:data.firstName,showResults:true});
-//     }
-    
-//     return (
-
-//         <View >
-           
-//             <Text style={styles.header}>Age Calculator</Text>
-//             <Controller
-//         control={control}
-//         rules={{
-//           required: true,
-//         }}
-//         render={({field: {onChange, onBlur, value}}) => (
-//           <TextInput
-//             placeholder="Enter Name"
-//             style={styles.input}
-//             onBlur={onBlur}
-//             onChangeText={onChange}
-//             value={value}
-//           />
-//         )}
-//         name="firstName"
-//       />
-//       {errors.firstName && <Text style={{color:'red'}}> ⚠This is required.</Text>}
-//             <View>
-//                 <Text style={styles.field}>Select Your Birthday</Text>
-//                 <View>
-              
-//             <DateField
-//             onSubmit={changeHandler}
-//             styleInput={{ fontSize: 20 }}
-//             containerStyle={{ marginVertical: 20, marginHorizontal: 70, borderWidth: 3, borderRadius: 10 }}
-//         />
-       
-   
-//                 </View>
-               
-//                 <View>
-//                     <TouchableOpacity
-//                         style={styles.button}
-                        
-//                         onPress= {handleSubmit(onSubmit,CalculateAge)}
-//                            >
-//                         <Text style={styles.buttonText}>Calculate</Text>
-//                     </TouchableOpacity>
-
-//                 </View>
-//                 <View>
-//                     {data.showResults ? <Results  date={data.birthday} /> : <View />}
-//                 </View>
-//             </View>
-          
-//         </View>
-
-//     );
-// }
-// const styles = StyleSheet.create({
-//     header: {
-//         backgroundColor: 'red',
-//         fontWeight: 'bold',
-//         fontSize: 32,
-//         paddingTop: 15,
-//         paddingBottom: 15,
-//         alignItems: "center",
-//         textAlign: "center",
-//         color: '#fff'
-//     },
-//     field: {
-//         fontSize: 22,
-//         textAlign: "center",
-//         marginTop: 30,
-//         color: "#202020"
-
-
-//     },
-//     button: {
-//         backgroundColor: '#0080FF',
-//         marginLeft: 20,
-//         marginRight: 20,
-//         paddingTop: 10,
-//         paddingBottom: 10,
-//         alignItems: "center",
-//         textAlign: "center",
-//         color: '#fff'
-//     },
-//     buttonText: {
-//         fontSize: 20,
-//         color: '#fff',
-//         fontWeight: 'bold'
-//     }
-
-
-
-// })
-// export default Calculator;
 
 import React, { useState } from 'react';
-import { Text, View, StyleSheet, TouchableOpacity,TextInput } from 'react-native'
+import { Text, View, StyleSheet, TouchableOpacity, TextInput } from 'react-native'
 import Results from './Results';
 import DateField from 'react-native-datefield';
-import {useForm, Controller} from 'react-hook-form';
+import { useForm, Controller } from 'react-hook-form';
 function Calculator() {
     const {
         control,
         handleSubmit,
-        formState: {errors},
-      } = useForm({
+        formState: { errors },
+    } = useForm({
         defaultValues: {
-          firstName: '',
-         
+            firstName: '',
+
         },
-      });
-      const onSubmit = data =>{
-          CalculateAge()
-          console.log(data)
-      }
+    });
+    const onSubmit = data => {
+        CalculateAge()
+        console.log(data)
+    }
 
     const [data, setData] = useState({
         date: '',
@@ -268,48 +142,48 @@ function Calculator() {
     const changeHandler = (value) => {
         setData({ date: value });
     };
-    
+
     return (
 
         <View >
-           
+
             <Text style={styles.header}>Age Calculator</Text>
             <Controller
-        control={control}
-        rules={{
-          required: true,
-        }}
-        render={({field: {onChange, onBlur, value}}) => (
-          <TextInput
-            placeholder="Enter Name"
-            style={styles.input}
-            onBlur={onBlur}
-            onChangeText={onChange}
-            value={value}
-          />
-        )}
-        name="firstName"
-      />
-      {errors.firstName && <Text style={{color:'red'}}> ⚠This is required.</Text>}
+                control={control}
+                rules={{
+                    required: true,
+                }}
+                render={({ field: { onChange, onBlur, value } }) => (
+                    <TextInput
+                        placeholder="Enter Name"
+                        style={styles.input}
+                        onBlur={onBlur}
+                        onChangeText={onChange}
+                        value={value}
+                    />
+                )}
+                name="firstName"
+            />
+            {errors.firstName && <Text style={{ color: 'red' }}> ⚠This is required.</Text>}
             <View>
                 <Text style={styles.field}>Select Your Birthday</Text>
                 <View>
-              
-            <DateField
-            onSubmit={changeHandler}
-            styleInput={{ fontSize: 20 }}
-            containerStyle={{ marginVertical: 20, marginHorizontal: 70, borderWidth: 3, borderRadius: 10 }}
-        />
-       
-   
+
+                    <DateField
+                        onSubmit={changeHandler}
+                        styleInput={{ fontSize: 20 }}
+                        containerStyle={{ marginVertical: 20, marginHorizontal: 70, borderWidth: 3, borderRadius: 10 }}
+                    />
+
+
                 </View>
-               
+
                 <View>
                     <TouchableOpacity
                         style={styles.button}
-                        
+
                         onPress={handleSubmit(onSubmit)}
-                           >
+                    >
                         <Text style={styles.buttonText}>Calculate</Text>
                     </TouchableOpacity>
 
@@ -318,7 +192,7 @@ function Calculator() {
                     {data.showResults ? <Results date={data.birthday} /> : <View />}
                 </View>
             </View>
-          
+
         </View>
 
     );
--- Components/Results.js
+++ Components/Results.js
@@ -14,7 +14,7 @@ function Results({ date }) {
     let seconds = Math.floor(days * 60 * 60 * 24);
     let years = Math.floor(days / 365);
     days -= years * 365;
-   
+
     let months = Math.floor(days / 31);
     days -= months * 31;
     if (today < birthdate) {
